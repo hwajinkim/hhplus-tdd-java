919a49213718ae863c13ef504a4597d3e746663f
hwa.j <hwajin0720@naver.com>
2024-12-18 22:58:19 +0900

    Feat: PointService 비즈니스 로직 작성 - 포인트 충전 메서드(예외처리 잘못된 인자, 객체 상태 문제) - 포인트 사용 메서드(예외처리 잘못된 인자, 객체 상태 문제)

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -31,4 +31,37 @@ public class PointService {
         }
         return pointHistories;
     }
+
+    public UserPoint patchPointCharge(long userId, long addAmount,long fixTime){
+        UserPoint userPoint = userPointTable.selectById(userId);
+        if(userPoint == null){
+            throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
+        }
+
+        long newPoint = userPoint.point() + addAmount;
+        if(newPoint > MAX_POINT){
+            throw new IllegalStateException("최대 잔고 초과로 포인트 충전에 실패하였습니다.");
+        }
+
+        UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
+        pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, fixTime);
+        return updatedPoint;
+    }
+
+    public UserPoint patchPointUse(long userId, long reduceAmount, long fixedTime){
+        UserPoint userPoint = userPointTable.selectById(userId);
+        if(userPoint == null){
+            throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
+        }
+
+        if(userPoint.point() < reduceAmount){
+            throw new IllegalStateException("잔고 포인트 부족으로 포인트 사용에 실패하였습니다.");
+        }
+
+        long newPoint = userPoint.point() - reduceAmount;
+
+        UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
+        pointHistoryTable.insert(userId, reduceAmount, TransactionType.USE, fixedTime);
+        return updatedPoint;
+    }
 }
