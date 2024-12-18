9d5d7a812a6e69393d335d1b87d6713b4cd67432
hwa.j <hwajin0720@naver.com>
2024-12-19 00:45:01 +0900

    Fix: mock 객체 접근자 지정

--- src/test/java/io/hhplus/tdd/point/unitTest/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/unitTest/PointServiceTest.java
@@ -15,6 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,10 +37,12 @@ public class PointServiceTest {
     private PointService pointService;
 
     @Mock
-    UserPoint mockUserPoint;
+    private UserPoint mockUserPoint;
 
     @Mock
-    List<PointHistory> mockPointHistory;
+    private List<PointHistory> mockPointHistory;
+
+    private long fixedTime;
 
     // 리팩토링 : 공통으로 쓰이는 기대값은 테스트 실행전에 생성
     @BeforeEach
@@ -47,6 +52,7 @@ public class PointServiceTest {
                 new PointHistory(1L, 1L, 700L, TransactionType.CHARGE, System.currentTimeMillis()),
                 new PointHistory(2L, 1L, 500L, TransactionType.USE, System.currentTimeMillis())
         );
+        fixedTime = 1700000000000L;
     }
     @Test
     @DisplayName("포인트 조회 테스트")
@@ -127,51 +133,76 @@ public class PointServiceTest {
 
     @Test
     @DisplayName("포인트 충전 테스트")
-        // 테스트 목적 : 충전 요청 포인트만큼 포인트가 증가하고, 반환값이 기대값과 일치하는지 확인
+    // 테스트 목적 : 충전 요청 포인트만큼 포인트가 증가하고, 반환값이 기대값과 일치하는지 확인
     void patchPointChargeTest(){
         //given
         long userId = 1L;
         long addAmount = 300L;
+
         // 포인트 충전으로 업데이트 된 데이터 기대값
         UserPoint updateUserPoint = new UserPoint(userId, mockUserPoint.point()+addAmount, System.currentTimeMillis());
+        // 포인트 히스토리에 충전 내역 저장된 기대값
+        PointHistory mockPointHistory = new PointHistory(1L, userId, addAmount, TransactionType.CHARGE, fixedTime);
 
         when(userPointTable.selectById(userId)).thenReturn(mockUserPoint);
         when(userPointTable.insertOrUpdate(userId,updateUserPoint.point())).thenReturn(updateUserPoint);
+        when(pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, fixedTime)).thenReturn(mockPointHistory);
 
         //when
-        UserPoint chargePoint = pointService.patchPointCharge(userId, addAmount);
+        UserPoint chargePoint = pointService.patchPointCharge(userId, addAmount, fixedTime);
         //then
         assertNotNull(chargePoint);
-        assertEquals(updateUserPoint.point(), chargePoint.point());
+        assertEquals(updateUserPoint, chargePoint);
         verify(userPointTable, times(1)).selectById(userId);
         verify(userPointTable, times(1)).insertOrUpdate(userId, updateUserPoint.point());
+        verify(pointHistoryTable, times(1)).insert( userId, addAmount, TransactionType.CHARGE, fixedTime);
     }
 
     @Test
+    @DisplayName("포인트 충전 실패테스트 - 해당 사용자 포인트 정보 없음")
+    // 테스트 목적 : userId로 포인트를 조회했을 때 해당 사용자 포인트 정보가 없으면 ,IllegalArgumentException이 발생하는지 확인
+    void patchPointChargeNotFoundUserFailTest(){
+        //given
+        long userId = 1L;
+        long addAmount = 300L;
+
+        when(userPointTable.selectById(userId)).thenReturn(null);
+        //when & then
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                ()-> pointService.patchPointCharge(userId, addAmount, fixedTime));
+
+        // 잘못된 인수로 발생한 예외 메세지가 예상 메세지와 일치하는지 체크
+        assertEquals("해당 사용자 포인트 정보를 찾을 수 없습니다.", exception.getMessage());
+        verify(userPointTable, times(1)).selectById(userId);
+        // 예외가 발생한 이후 포인트 충전 업데이트, 포인트 내역이 기록되지 않았는지 체크
+        verify(userPointTable, never()).insertOrUpdate(userId, mockUserPoint.point()+addAmount);
+        verify(pointHistoryTable, never()).insert(userId, addAmount, TransactionType.CHARGE, fixedTime);
+    }
+    @Test
     @DisplayName("포인트 충전 실패테스트 - 최대 잔고 초과")
-        // 테스트 목적 : 충전 요청 포인트와 현재 포인트의 합이 최대 잔고 포인트를 초과할 때, IllegalArgumentException이 발생하는지 확인
-    void patchPointChargExceededBalanceFailTest(){
+    // 테스트 목적 : 충전 요청 포인트와 현재 포인트의 합이 최대 잔고 포인트를 초과할 때, IllegalStateException이 발생하는지 확인
+    void patchPointChargeExceededBalanceFailTest(){
         //given
         // 충전할 포인트(400) + 현재 포인트(700)가 최대 잔고(1000)보다 크도록 설정
         long userId = 1L;
         long addAmount = 400L;
+
         when(userPointTable.selectById(userId)).thenReturn(mockUserPoint);
 
         //when & then
-        Exception exception = assertThrows(IllegalArgumentException.class,
-                ()-> pointService.patchPointCharge(userId, addAmount));
+        Exception exception = assertThrows(IllegalStateException.class,
+                ()-> pointService.patchPointCharge(userId, addAmount, fixedTime));
 
         // 최대 잔고 초과로 발생한 예외 메세지가 예상 메세지와 일치하는지 체크
         assertEquals("최대 잔고 초과로 포인트 충전에 실패하였습니다.", exception.getMessage());
         verify(userPointTable, times(1)).selectById(userId);
-        // 예외가 발생한 이후에 포인트 충전 업데이트와 포인트 내역 기록이 되지 않았는지 체크
         verify(userPointTable, never()).insertOrUpdate(userId, mockUserPoint.point()+addAmount);
-        verify(pointHistoryTable, never()).insert(userId, addAmount, TransactionType.CHARGE, System.currentTimeMillis());
+        verify(pointHistoryTable, never()).insert(userId, addAmount, TransactionType.CHARGE, fixedTime);
     }
 
     @Test
     @DisplayName("포인트 사용 테스트")
-        // 테스트 목적 : 사용 요청 포인트만큼 포인트가 감소하고, 반환값이 기대값과 일치하는지 확인
+    // 테스트 목적 : 사용 요청 포인트만큼 포인트가 감소하고, 반환값이 기대값과 일치하는지 확인
     void patchPointUse(){
         //given
         long userId = 1L;
@@ -179,11 +210,15 @@ public class PointServiceTest {
 
         // 포인트 사용으로 업데이트 된 데이터 기대값
         UserPoint updateUserPoint = new UserPoint(userId, mockUserPoint.point()-reduceAmount, System.currentTimeMillis());
+        // 포인트 히스토리에 충전 내역 저장된 기대값
+        PointHistory mockPointHistory = new PointHistory(1L, userId, reduceAmount, TransactionType.USE, fixedTime);
+
         when(userPointTable.selectById(userId)).thenReturn(mockUserPoint);
         when(userPointTable.insertOrUpdate(userId, updateUserPoint.point())).thenReturn(updateUserPoint);
+        when(pointHistoryTable.insert( userId, reduceAmount, TransactionType.USE, fixedTime)).thenReturn(mockPointHistory);
 
         //when
-        UserPoint usePoint = pointService.patchPointUse(userId, reduceAmount);
+        UserPoint usePoint = pointService.patchPointUse(userId, reduceAmount, fixedTime);
 
         //then
         assertNotNull(usePoint);
@@ -191,27 +226,50 @@ public class PointServiceTest {
         assertEquals(updateUserPoint.point(), usePoint.point());
         verify(userPointTable, times(1)).selectById(userId);
         verify(userPointTable, times(1)).insertOrUpdate(userId, updateUserPoint.point());
+        verify(pointHistoryTable, times(1)).insert(userId, reduceAmount, TransactionType.USE, fixedTime);
+    }
+
+    @Test
+    @DisplayName("포인트 사용 실패 테스트 - 해당 사용자 포인트 정보 없음")
+    // 테스트 목적 : userId로 포인트를 조회했을 때 해당 사용자 포인트 정보가 없으면 ,IllegalArgumentException이 발생하는지 확인
+    void patchPointUseNotFoundUserFailTest(){
+        //given
+        long userId = 1L;
+        long reduceAmount = 300L;
+
+        when(userPointTable.selectById(userId)).thenReturn(null);
+
+        //when & then
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                ()-> pointService.patchPointUse(userId, reduceAmount, fixedTime));
+
+        // 잘못된 인수로 발생한 예외 메세지가 예상 메세지와 일치하는지 체크
+        assertEquals("해당 사용자 포인트 정보를 찾을 수 없습니다.", exception.getMessage());
+        verify(userPointTable, times(1)).selectById(userId);
+        verify(userPointTable, never()).insertOrUpdate(userId,reduceAmount);
+        verify(pointHistoryTable, never()).insert(userId, reduceAmount, TransactionType.USE, fixedTime);
     }
 
     @Test
     @DisplayName("포인트 사용 실패 테스트 - 잔고 부족")
-        // 테스트 목적 : 사용 요청 포인트가 현재 포인트보다 클 때, IllegalArgumentException이 발생하는지 확인
+    // 테스트 목적 : 사용 요청 포인트가 현재 포인트보다 클 때, IllegalStateException이 발생하는지 확인
     void patchPointUseLackedBalanceFailTest(){
         //given
         // 사용 요청 포인트가 현재 포인트보다 크도록 설정
         long userId = 1L;
         long reduceAmount = 1000L;
+
         when(userPointTable.selectById(userId)).thenReturn(mockUserPoint);
 
         //when & then
-        Exception exception = assertThrows(IllegalArgumentException.class,
-                ()-> pointService.patchPointUse(userId, reduceAmount));
+        Exception exception = assertThrows(IllegalStateException.class,
+                ()-> pointService.patchPointUse(userId, reduceAmount, fixedTime));
 
         // 잔고 부족으로 발생한 예외 메세지와 예상 메세지가 일치하는지 체크
         assertEquals("잔고 포인트 부족으로 포인트 사용에 실패하였습니다.", exception.getMessage());
         verify(userPointTable, times(1)).selectById(userId);
         // 잔고 부족 예외가 발생한 이후에 포인트 사용 업데이트와 포인트 내역 기록이 되지 않는지 체크
         verify(userPointTable, never()).insertOrUpdate(userId, mockUserPoint.point()-reduceAmount);
-        verify(pointHistoryTable, never()).insert(userId, reduceAmount, TransactionType.CHARGE, System.currentTimeMillis());
+        verify(pointHistoryTable, never()).insert(userId, reduceAmount, TransactionType.USE, fixedTime);
     }
 }
