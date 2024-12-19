c27ac06bab169352b3371a02b9b7d420662a6598
hwa.j <hwajin0720@naver.com>
2024-12-19 23:47:57 +0900

    Test: 동시성 제어 통합테스트 작성 - 동시에 10개의 스레드가 포인트 충전 및 사용 시 발생하는 동시성 이슈 제어 - ExecutorService를 사용하여 순서 보장, ReentrantLock을 사용하여 명시적으로 lock 생성/해제로 동시성 제어 - pointService의  메서드 진입 시 lock 사용

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -17,6 +17,10 @@ public class PointService {
     private final PointHistoryTable pointHistoryTable;
     private final PointValidation pointValidation;
 
+    // ReentrantLock 생성자의 매개변수를 true 전달
+    // => 가장 오래 기다린 쓰레드가 lock을 획득할 수 있게 공정(fair)하게 처리
+    private ReentrantLock lock = new ReentrantLock(true);
+
     private Long MAX_POINT = 1000L;
 
     public UserPoint getUserPoint(long userId){
@@ -38,39 +42,52 @@ public class PointService {
     }
 
     public UserPoint patchPointCharge(long userId, long addAmount,long fixTime){
-        pointValidation.validateUserId(userId);
-        pointValidation.validateAmount(addAmount);
-        UserPoint userPoint = userPointTable.selectById(userId);
-        if(userPoint == null){
-            throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
-        }
+        // .lock() 메서드를 통해 락을 획득
+        // .unlock()을 사용하여 하나의 스레드 실행 후엔 무조건 락을 해제하여 교착상태를 방지한다.
+        // try-finally를 사용하여 예외가 발생해도 항상 unlock() 호출
+        lock.lock();
+        try{
+            pointValidation.validateUserId(userId);
+            pointValidation.validateAmount(addAmount);
+            UserPoint userPoint = userPointTable.selectById(userId);
+            if(userPoint == null){
+                throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
+            }
 
-        long newPoint = userPoint.point() + addAmount;
-        if(newPoint > MAX_POINT){
-            throw new IllegalStateException("최대 잔고 초과로 포인트 충전에 실패하였습니다.");
-        }
+            long newPoint = userPoint.point() + addAmount;
+            if(newPoint > MAX_POINT){
+                throw new IllegalStateException("최대 잔고 초과로 포인트 충전에 실패하였습니다.");
+            }
 
-        UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
-        pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, fixTime);
-        return updatedPoint;
+            UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
+            pointHistoryTable.insert(userId, addAmount, TransactionType.CHARGE, fixTime);
+            return updatedPoint;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public UserPoint patchPointUse(long userId, long reduceAmount, long fixedTime){
-        pointValidation.validateUserId(userId);
-        pointValidation.validateAmount(reduceAmount);
-        UserPoint userPoint = userPointTable.selectById(userId);
-        if(userPoint == null){
-            throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
+        lock.lock();
+        try{
+            pointValidation.validateUserId(userId);
+            pointValidation.validateAmount(reduceAmount);
+            UserPoint userPoint = userPointTable.selectById(userId);
+            if(userPoint == null){
+                throw new IllegalArgumentException("해당 사용자 포인트 정보를 찾을 수 없습니다.");
+            }
+
+            if(userPoint.point() < reduceAmount){
+                throw new IllegalStateException("잔고 포인트 부족으로 포인트 사용에 실패하였습니다.");
+            }
+
+            long newPoint = userPoint.point() - reduceAmount;
+
+            UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
+            pointHistoryTable.insert(userId, reduceAmount, TransactionType.USE, fixedTime);
+            return updatedPoint;
+        } finally {
+            lock.unlock();
         }
-
-        if(userPoint.point() < reduceAmount){
-            throw new IllegalStateException("잔고 포인트 부족으로 포인트 사용에 실패하였습니다.");
-        }
-
-        long newPoint = userPoint.point() - reduceAmount;
-
-        UserPoint updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
-        pointHistoryTable.insert(userId, reduceAmount, TransactionType.USE, fixedTime);
-        return updatedPoint;
     }
 }
